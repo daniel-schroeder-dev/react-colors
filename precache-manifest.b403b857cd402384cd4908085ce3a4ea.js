3de92f21b47440be60ff2c4ac8bc2b1a35493880
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-15 20:12:04 -0500

    Updates

---
+++ precache-manifest.b403b857cd402384cd4908085ce3a4ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66570d723e1bb1331460654f55e344d",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "8eb6372ccd3071c6701a",
+    "url": "/react-colors/static/css/main.f2d14137.chunk.css"
+  },
+  {
+    "revision": "ba8a58f436aee6483a99",
+    "url": "/react-colors/static/js/2.5b38dd22.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-colors/static/js/2.5b38dd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eb6372ccd3071c6701a",
+    "url": "/react-colors/static/js/main.9a28caed.chunk.js"
+  },
+  {
+    "revision": "683c161634517a6618c1",
+    "url": "/react-colors/static/js/runtime-main.f65ccff2.js"
+  }
+]);
